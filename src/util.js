b56676c4d64b15d0e4a0405da5f85cf00cb593c0
Furybean <long.zhang@ele.me>
2016-12-04 19:44:14 +0800

    add getPageURL for web & native

--- src/util.js
+++ src/util.js
@@ -2,3 +2,11 @@ export const getImageURL = function(hash) {
   const urlPart = hash.replace(/^(\w)(\w\w)(\w{29}(\w*))$/, '/$1/$2/$3.$4');
   return `https://fuss10.elemecdn.com/${urlPart}?imageMogr/format/jpeg/thumbnail/184x/`;
 };
+
+export const getPageLink = function(page, config) {
+  if (typeof window !== 'undefined') {
+    return `/web/?page=../dist/${page}.js`;
+  }
+  const pageName = config.bundleUrl;
+  return pageName.replace(/\/([^\.^/]+)\.js.*$/g, `/${page}.js?random=${Date.now()}`);
+};
