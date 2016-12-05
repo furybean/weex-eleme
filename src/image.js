1033da69ce08d1794df6f24a4f9f5afb7ed985a7
Furybean <long.zhang@ele.me>
2016-12-06 01:44:00 +0800

    add profile page

--- src/image.js
+++ src/image.js
@@ -1,5 +1,15 @@
 export default {
   shop: require('./images/shop.png'),
   orangeStar: require('./images/orange-star.png'),
-  grayStar: require('./images/gray-star.png')
+  grayStar: require('./images/gray-star.png'),
+  balance: require('./images/balance.png'),
+  coins: require('./images/coins.png'),
+  eleme: require('./images/eleme.png'),
+  luckybag: require('./images/luckybag.png'),
+  mobile: require('./images/mobile.png'),
+  redbag: require('./images/redbag.png'),
+  service: require('./images/service.png'),
+  vip: require('./images/vip.png'),
+  profile: require('./images/profile.png'),
+  order: require('./images/order.png')
 };
