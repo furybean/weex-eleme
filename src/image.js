a351e5ced9120a48bace182acde6ad692e3c24a4
Furybean <long.zhang@ele.me>
2016-12-06 16:21:23 +0800

    change bottom tab images

--- src/image.js
+++ src/image.js
@@ -11,5 +11,13 @@ export default {
   service: require('./images/service.png'),
   vip: require('./images/vip.png'),
   profile: require('./images/profile.png'),
-  order: require('./images/order.png')
+  order: require('./images/order.png'),
+  tabDiscovery: require('./images/tab_discovery.png'),
+  tabDiscoverySelected: require('./images/tab_discovery_selected.png'),
+  tabOrder: require('./images/tab_order.png'),
+  tabOrderSelected: require('./images/tab_order_selected.png'),
+  tabShopping: require('./images/tab_shopping.png'),
+  tabShoppingSelected: require('./images/tab_shopping_selected.png'),
+  tabUser: require('./images/tab_user.png'),
+  tabUserSelected: require('./images/tab_user_selected.png')
 };
